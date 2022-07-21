b4380515c3c264695a530ebd3e1abdc29765cb74
CalebAurand <caurand13@gmail.com>
2022-07-20 22:09:30 -0500

    mapped over todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor (props){
+    super(props);
+
+    this.state = {
+      isClicked: false,
+      todos: [],
+      text: '',
+    }
+  }
+
+  onClickHandler = (e) => {
+      e.preventDefault();
+      this.setState({
+        isClicked: !this.state.isClicked
+      });
+      this.setState({
+        todos: [...this.state.todos, 
+          this.state.text],
+      text: ''
+    });
+      console.log(this.state.isClicked);
+      e.target.value = '';
+  }
+  
+  onChangeHandler = (e) => {
+    this.setState({text: e.target.value})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form>
+            <input type="text" placeholder='todo' onChange={this.onChangeHandler} value={this.state.text}></input>
+            <button onClick={(this.onClickHandler)}>Set Todo</button>
+          </form>
+          <ul id="list">
+            {this.state.todos.map((e, i)=>{
+              return <li key={i}>{this.state.todos[i]}<button>Delete</button></li>
+            })}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
